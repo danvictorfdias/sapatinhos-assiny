d9233df87aae0e71c4fde1215f6ae09d682ac662
Daniel <danvictorfdias@gmail.com>
2025-12-05 15:02:17 -0300

    alteração da faixa do banner

--- src/components/topbanner/TopBanner.tsx
+++ src/components/topbanner/TopBanner.tsx
@@ -9,7 +9,7 @@ function TopBanner() {
 
     return (
         <div className="bg-black text-white text-center py-2 px-4 text-sm md:text-base font-medium">
-            Desconto de Black Friday, só <span className="font-bold" style={{ color: '#FDD835' }}>HOJE</span> - <span className="font-bold" style={{ color: '#FDD835' }}>{formattedDate}</span>
+            Último dia com preço especial. Somente <span className="font-bold" style={{ color: '#FDD835' }}>HOJE</span>, <span className="font-bold" style={{ color: '#FDD835' }}>{formattedDate}</span>.
         </div>
     );
 }
